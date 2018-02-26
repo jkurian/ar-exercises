80a10954a6be0f425394ec6d76450759a740ad00
Jerry Kurian <jkurian770@gmail.com>
2018-02-26 15:16:37 -0500

    Printing errors in ex7 and updated validations

--- lib/store.rb
+++ lib/store.rb
@@ -1,12 +1,17 @@
+class ValidateApparel < ActiveModel::Validator
+    def validate(record)
+        unless record.mens_apparel || record.womens_apparel
+            #not sure how to add error properly
+            record.errors[:required] << 'to sell mens or womens apparel'
+        end
+    end
+end
+
 class Store < ActiveRecord::Base
+    include ActiveModel::Validations
+
     has_many :employees
     validates :name, length: { minimum: 3 }
     validates_numericality_of :annual_revenue, :only_integer => true, :greater_than_or_equal_to => 0
-    validate :must_sell_mens_or_womens_apparel
-
-    def must_sell_mens_or_womens_apparel
-        if(!mens_apparel && !womens_apparel)
-            errors.add(:mens_apparel, :womens_apparel, 'Must sell either mens or womens apparel')
-        end
-    end
+    validates_with ValidateApparel
 end
