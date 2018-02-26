3c81fa62302b7659a54484c339038501edce2379
Jerry Kurian <jkurian770@gmail.com>
2018-02-26 14:43:25 -0500

    Added validations to employees and stores

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,5 @@
 class Employee < ActiveRecord::Base
     belongs_to :store
+    validates_presence_of :first_name, :last_name, :store_id
+    validates :hourly_rate, :inclusion => { :in => 40..200 }
 end
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,12 @@
 class Store < ActiveRecord::Base
     has_many :employees
+    validates :name, length: { minimum: 3 }
+    validates_numericality_of :annual_revenue, :only_integer => true, :greater_than_or_equal_to => 0
+    validate :must_sell_mens_or_womens_apparel
+
+    def must_sell_mens_or_womens_apparel
+        if(!mens_apparel && !womens_apparel)
+            errors.add(:mens_apparel, :womens_apparel, 'Must sell either mens or womens apparel')
+        end
+    end
 end
