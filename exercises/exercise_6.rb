8e0033bf8a30af75a669f973b78ce91f3d424c58
Jerry Kurian <jkurian770@gmail.com>
2018-02-26 14:27:52 -0500

    Seeded employees to store 1 and 2. Added relation between stores and employees

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,24 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.first
+@store1.employees.create([
+    { 
+        first_name: 'John', 
+        last_name: 'White', 
+        hourly_rate: 15, 
+    },
+    { 
+        first_name: 'Betty', 
+        last_name: 'Doe', 
+        hourly_rate: 17, 
+    },
+    { 
+        first_name: 'Jerry', 
+        last_name: 'Kurian', 
+        hourly_rate: 150, 
+    }
+])
+
+@store2 = Store.second
+@store2.employees.create({first_name: "Bob", last_name:"Generic", hourly_rate: 16})
