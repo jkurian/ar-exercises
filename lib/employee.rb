43730c4cc2557a2de2df54a59529b3b6d03d616d
Jerry Kurian <jkurian770@gmail.com>
2018-02-26 15:54:49 -0500

    Added password to employees whcih is randomly generated with after_create cb

--- lib/employee.rb
+++ lib/employee.rb
@@ -2,4 +2,10 @@ class Employee < ActiveRecord::Base
     belongs_to :store
     validates_presence_of :first_name, :last_name, :store_id
     validates :hourly_rate, :inclusion => { :in => 40..200 }
+    
+    private
+    after_create do 
+        pw = (0...8).map { (65 + rand(26)).chr }.join
+        Employee.find(self.id).update(password: pw)
+    end
 end
